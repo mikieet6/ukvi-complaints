d7e4afaeefedc5c92c69e5b7951bb96006467f9a
Ben Marvell <madmarv187@gmail.com>
2020-03-29 14:23:36 +0100

    Render data in emails correctly

--- apps/ukvi-complaints/behaviours/customer-email.js
+++ apps/ukvi-complaints/behaviours/customer-email.js
@@ -9,12 +9,108 @@ const getDataRows = (model, translate) => {
     {
       title: 'Complaint details',
       table: [
-        {
-          label: 'The reason for the complaint',
+        model.reason && {
+          label: translate('pages.confirm.fields.reason.label'),
           value: translate(`fields.reason.options[${model.reason}].label`)
         },
-        {
-          label: 'Complaint details',
+        model['immigration-application'] && {
+          label: translate('pages.confirm.fields.immigration-application.label'),
+          value: translate(`fields.immigration-application.options[${model['immigration-application']}].label`)
+        },
+        model['immigration-appointment'] && {
+          label: translate('pages.confirm.fields.immigration-appointment.label'),
+          value: translate(`fields.immigration-appointment.options[${model['immigration-appointment']}].label`)
+        },
+        model['delay-type'] && {
+          label: translate('pages.confirm.fields.delay-type.label'),
+          value: translate(`fields.delay-type.options[${model['delay-type']}].label`)
+        },
+        model['return-of-documents'] && {
+          label: translate('pages.confirm.fields.return-of-documents.label'),
+          value: translate(`fields.return-of-documents.options[${model['return-of-documents']}].label`)
+        },
+        model['decision-outcome'] && {
+          label: translate('pages.confirm.fields.decision-outcome.label'),
+          value: translate(`fields.decision-outcome.options[${model['decision-outcome']}].label`)
+        },
+        model['biometric-residence-permit'] && {
+          label: translate('pages.confirm.fields.biometric-residence-permit.label'),
+          value: translate(`fields.biometric-residence-permit.options[${model['biometric-residence-permit']}].label`)
+        },
+        model['poor-info-or-behaviour'] && {
+          label: translate('pages.confirm.fields.poor-info-or-behaviour.label'),
+          value: translate(`fields.poor-info-or-behaviour.options[${model['poor-info-or-behaviour']}].label`)
+        },
+        model['staff-behaviour'] && {
+          label: translate('pages.confirm.fields.staff-behaviour.label'),
+          value: translate(`fields.staff-behaviour.options[${model['staff-behaviour']}].label`)
+        },
+        model['which-centre'] && {
+          label: translate('pages.confirm.fields.which-centre.label'),
+          value: translate(`fields.which-centre.options[${model['which-centre']}].label`)
+        },
+        model.refund && {
+          label: translate('pages.confirm.fields.refund.label'),
+          value: translate(`fields.refund.options[${model.refund}].label`)
+        },
+        model['refund-when'] && {
+          label: translate('pages.confirm.fields.refund-when.label'),
+          value: translate(`fields.refund-when.options[${model['refund-when']}].label`)
+        },
+        model['refund-type'] && {
+          label: translate('pages.confirm.fields.refund-type.label'),
+          value: translate(`fields.refund-type.options[${model['refund-type']}].label`)
+        },
+        model['where-applied-from'] && {
+          label: translate('pages.confirm.fields.where-applied-from.label'),
+          value: translate(`fields.where-applied-from.options[${model['where-applied-from']}].label`)
+        },
+        model['existing-complaint'] && {
+          label: translate('pages.confirm.fields.existing-complaint.label'),
+          value: translate(`fields.existing-complaint.options[${model['existing-complaint']}].label`)
+        },
+        model['complaint-reason-previous'] && {
+          label: translate('pages.confirm.fields.complaint-reason-previous.label'),
+          value: translate(`fields.complaint-reason-previous.options[${model['complaint-reason-previous']}].label`)
+        },
+        model['ssc-city'] && {
+          label: translate('pages.confirm.fields.ssc-city.label'),
+          value: model['ssc-city']
+        },
+        model['vac-country'] && {
+          label: translate('pages.confirm.fields.vac-country.label'),
+          value: model['vac-country']
+        },
+        model['vac-city'] && {
+          label: translate('pages.confirm.fields.vac-city.label'),
+          value: model['vac-city']
+        },
+        model['ukvcas-city'] && {
+          label: translate('pages.confirm.fields.ukvcas-city.label'),
+          value: model['ukvcas-city']
+        },
+        model['complaint-reference-number'] && {
+          label: translate('pages.confirm.fields.complaint-reference-number.label'),
+          value: model['complaint-reference-number']
+        },
+        model['gwf-reference'] && {
+          label: translate('pages.confirm.fields.gwf-reference.label'),
+          value: model['gwf-reference']
+        },
+        model['ho-reference'] && {
+          label: translate('pages.confirm.fields.ho-reference.label'),
+          value: model['ho-reference']
+        },
+        model['ihs-reference'] && {
+          label: translate('pages.confirm.fields.ihs-reference.label'),
+          value: model['ihs-reference']
+        },
+        model['uan-reference'] && {
+          label: translate('pages.confirm.fields.uan-reference.label'),
+          value: model['uan-reference']
+        },
+        model['complaint-details'] && {
+          label: translate('pages.confirm.fields.complaint-details.label'),
           value: model['complaint-details']
         }
       ]
